d9484d802b17021e822288c72dc2c3779c6d6c4f
Nisha282/training <sahunishaji1220@gmail.com>
2022-10-11 20:22:13 +0530

    create use done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const bcrypt = require ('bcrypt')
 const saltRound=10
 const userModel = require('../models/userModel')
@@ -8,6 +8,22 @@ const isValid = function(value){
     if(typeof value == 'string' && value.trim().length === 0) return false
     return true
   }
+   const isValidAddress = function (data) {
+    if (typeof (data) === "undefined" ||data === null) return false;
+    if (typeof (data) === "object" && Array.isArray(data) === false && Object.keys(data).length > 0) return true;
+    return false;
+  };
+  
+  /* ---------------------------------------pincode format------------------------------------------- */
+  const isValidPincode = function(data){
+    if ((/^[1-9][0-9]{5}$/.test(data))) {
+      return true
+    }
+    return false
+  }
+   const isValidOnlyCharacters = function (data) {
+    return /^[A-Za-z ]+$/.test(data)
+  }
   const isNotEmpty=function(value){
     if(value.trim().length!=0)
     return true; 
@@ -16,41 +32,39 @@ const isValid = function(value){
   const phoneregex = /^([6-9]\d{9})$/
 const emailregex = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/
 const passwordregex = /^[a-zA-Z0-9!@#$%^&*]{8,15}$/
-const pinregex=       /^\d{6}$/
-const streetregex=/^[0-9\\\/# ,a-zA-Z]+[ ,]+[0-9\\\/#, a-zA-Z]{1,}$/
+// const pinregex=       /^\d{6}$/
+// const streetregex=/^[0-9\\\/# ,a-zA-Z]+[ ,]+[0-9\\\/#, a-zA-Z]{1,}$/
 
 
+
+
+
+/* ------------------------------------------------aws config -------------------------------------------------------- */
 aws.config.update({
-    accessKeyId: "AKIAY3L35MCRZNIRGT6N",
-    secretAccessKey: "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",
+    accessKeyId: "AKIAY3L35MCRUJ6WPO6J",
+    secretAccessKey: "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",
     region: "ap-south-1"
-})
-
-let uploadFile= async ( file) =>{
-   return new Promise( function(resolve, reject) {
-    // this function will upload file to aws and return the link
-    let s3= new aws.S3({apiVersion: '2006-03-01'}); // we will be using the s3 service of aws
-
-    var uploadParams= {
+  });
+  
+  /* ------------------------------------------------aws fileUpload-------------------------------------------------------- */
+  let uploadFile = async (file) => {
+    return new Promise(function (resolve, reject) {
+      let s3 = new aws.S3({ apiVersion: "2006-03-01" });
+  
+      var uploadParams = {
         ACL: "public-read",
-        Bucket: "classroom-training-bucket",  //HERE
-        Key: "abc/" + file.originalname, //HERE 
-        Body: file.buffer
-    }
+        Bucket: "classroom-training-bucket",
+        Key: "user/" + file.originalname,
+        Body: file.buffer,
+      };
+      s3.upload(uploadParams, function (err, data) {
+        if (err) return reject({ error: err });
+  
+        return resolve(data.Location);
+      });
+    });
+  };
 
-
-    s3.upload( uploadParams, function (err, data ){
-        if(err) {
-            return reject({"error": err})
-        }
-        // console.log(data)
-        // console.log("file uploaded succesfully")
-        return resolve(data.Location)
-    })
-
-   
-   })
-}
 
 const createUser = async function(req, res){
 
@@ -58,13 +72,8 @@ const createUser = async function(req, res){
         let requestbody = req.body
         let requestQuery = req.query
         let files=req.files
-        if(files && files.length>0){
-          req.profileImage=await uploadFile(files[0])
-          console.log(req.profileImage)
-        }else
-          return res.
-            status(400).
-              send({status:false,msg:"invalid request"})
+        let fileUrl = await uploadFile(files[0]);
+    requestbody.profileImage = fileUrl;
         if (Object.keys(requestbody).length == 0)
             return res.
                 status(400).
@@ -101,78 +110,53 @@ const createUser = async function(req, res){
               return res.status(400).send({ status: false, message: "password is required" })
           if (!password.match(passwordregex))
               return res.status(400).send({ status: false, message: "password should be valid" })
-         let encryptPassword=await bcrypt.hash(password,saltRound)
-          requestbody.password=encryptPassword
-       if (!isValid(address)) {
-        if (Object.keys(address).length == 0) return res.status(400).send({ status: false, message: "Address must contain shipping,billing address" })
-        else {
-            const { shipping, billing } = address
-            if (!(isValid(shipping) || isValid(billing) )) return res.status(400).send({ status: false, message: "We are looking for sipping,billing value only inside Address Object" })
-            else {
-                if (address.shipping) {
-                    if (Object.keys(shipping).length == 0) return res.status(400).send({ status: false, message: "Address must contain street, city, pincode" })
-                    else {
-                        const { street, city, pincode } = shipping
-                        if (!(isValid(street) || isValid(city) || isValid(pincode))) return res.status(400).send({ status: false, message: "We are looking for street ,city or pincode value only inside Address Object" })
-                        else {
-                            if (street) {
-                                if (!isValid(street)) return res.status(400).send({ status: false, message: "street field is empty" });
-                                if (!street.match(streetregex)) return res.status(400).send({ status: false, message: "street is invalid" })
-                                address.street = street.trim()
-                            } 
-                            if (city) {
-                                if (!isNotEmpty(city)) return res.status(400).send({ status: false, message: "city field is empty" });
-                                if (!city.match(nameregex)) return res.status(400).send({ status: false, message: "city name is not valid" })
-                                address.city = city.trim()
-                            } if (pincode) {
-                                if (!isNotEmpty(pincode)) return res.status(400).send({ status: false, message: "pincode field is empty" });
-                                if (!pincode.match(pinregex)) return res.status(400).send({ status: false, message: "pincode must contain only digit with 6 length" })
-                                address.pincode = pincode.trim()
-                            }
-                        }
-                    }
-                }
-                if (address.billing) {
-                    if (Object.keys(billing).length == 0) return res.status(400).send({ status: false, message: "Address must contain street, city, pincode" })
-                    else {
-                        const { street, city, pincode } = billing
-                        if (!(isValid(street) || isValid(city) || isValid(pincode))) return res.status(400).send({ status: false, message: "We are looking for street ,city or pincode value only inside Address Object" })
-                        else {
-                            if (street) {
-                                if (!isValid(street)) return res.status(400).send({ status: false, message: "street field is empty" });
-                                if (!street.match(streetregex)) return res.status(400).send({ status: false, message: "street is invalid" })
-                                address.street = street.trim()
-                            } 
-                            if (city) {
-                                if (!isNotEmpty(city)) return res.status(400).send({ status: false, message: "city field is empty" });
-                                if (!city.match(nameregex)) return res.status(400).send({ status: false, message: "city name is not valid" })
-                                address.city = city.trim()
-                            } if (pincode) {
-                                if (!isNotEmpty(pincode)) return res.status(400).send({ status: false, message: "pincode field is empty" });
-                                if (!pincode.match(pinregex)) return res.status(400).send({ status: false, message: "pincode must contain only digit with 6 length" })
-                                address.pincode = pincode.trim()
-                            }
-                        }
-                    }
-                }
-            }console.log(billing)
-        }
-    }
-      
+              let encryptedPassword = bcrypt
+              .hash(requestbody.password, saltRound)
+              .then((hash) => {
+                console.log(`Hash: ${hash}`);
+                return hash;
+              });
         
-        let filterData={
-
-            password:encryptPassword,
-            profileImage: req.profileImage,
-            lname:lname,
-            fname:fname,
-            address:address,
-            phone:phone,
-            email:email
-            
+            requestbody.password = await encryptedPassword;
+       //===========================================ADDRESS==============================================
+       if (!isValidAddress(address)) {
+        return res
+          .status(400)
+          .send({ status: false, message: "Address is required!" });
+      }
+  
+      let arr1 = ["shipping", "billing"];
+      let arr2 = ["street", "city", "pincode"];
+      for (let i = 0; i < arr1.length; i++) {
+        if (!requestbody.address[arr1[i]])
+          return res
+            .status(400)
+            .send({ status: false, msg: `${arr1[i]} is mandatory` });
+        for (let j = 0; j < arr2.length; j++) {
+          if (!requestbody.address[arr1[i]][arr2[j]])
+            return res.status(400).send({
+              status: false,
+              msg: `In  ${arr1[i]}, ${arr2[j]} is mandatory`,
+            });
         }
-        console.log(filterData)
-        const result = await userModel.create(filterData)
+  
+        if (!isValidOnlyCharacters(requestbody.address[arr1[i]].city)) {
+          return res.status(400).send({
+            status: false,
+            message: `In ${arr1[i]} , city is invalid`,
+          });
+        }
+  
+        if (!isValidPincode(requestbody.address[arr1[i]].pincode)) {
+          return res.status(400).send({
+            status: false,
+            message: `In ${arr1[i]} , pincode is invalid`,
+          });
+        }
+      }
+       
+      
+      const result = await userModel.create(requestbody)
         console.log(result)
        
        return res.
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,24 +1,76 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
+    fname: {
+      type: String,
+      required: true
+    },
 
-    fname: { type: String, required: true },
-    lname: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    profileImage: { type: String, required: true },
-    password: { type: String, required: true },
-    phone:{type:String,required:true,unique:true},
+    lname: {
+      type: String,
+      required: true
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    profileImage: {
+      type: String,
+      required: true
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
     address: {
-        shipping: {
-            street: { type: String, required: true },
-            city: { type: String, required: true },
-            pincode: { type: Number, required: true }
+
+      shipping: {
+        street: {
+          type: String,
+           required: true
         },
-        billing: {
-            street: { type: String, required: true },
-            city: { type: String, required: true },
-            pincode: { type: Number, required: true }
+
+        city: {
+          type: String,
+           required: true
+        },
+
+        pincode: {
+          type: Number,
+            required: true
         }
+      },
+      billing: {
+        street: {
+          type: String,
+           required: true
+        },
+
+        city: {
+          type: String,
+           required: true
+        },
+
+        pincode: {
+          type: Number,
+            required: true
+        }
+      }
     }
-}, { timestamps: true })
-module.exports = mongoose.model("User", userSchema)
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("user", userSchema);
